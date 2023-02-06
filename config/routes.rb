4841716f83bfdaa238ec51da020601c8d0231fc3
Abby St. Germain <abbysaintg@gmail.com>
2023-02-06 16:52:53 -0500

    Configured client-side routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   # route to test your configuration
   get '/hello', to: 'application#hello_world'
+
+  # handles all other GET requests by sending them to a special FallbackController
+  get '*path',
+      to: 'fallback#index',
+      constraints: ->(req) { !req.xhr? && req.format.html? }
 end
